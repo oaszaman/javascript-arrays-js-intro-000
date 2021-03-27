f1975fb17c7dade401334aa47df947f479c6451c
Shahzab Zaman <o.aszaman00@gmail.com>
2021-03-27 23:42:03 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,7 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 
 function addElementToBeginningOfArray(chocolateBars, candyString){
-chocolateBars.unshift(chocolateBars);
+
   return [candyString, chocolateBars];
 }
 
