5bf2dbe4de79e9f8ff053c815690703c76b1fc0a
Shahzab Zaman <o.aszaman00@gmail.com>
2021-03-27 23:11:14 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,35 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(array, element) {
+addElementToBeginningOfArray.unshift();
+return addElementToBeginningOfArray();
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+destructivelyAddElementToBeginningOfArray.unshift();
+
+}
+
+function addElementToEndOfArray() {
+addElementToEndOfArray.push();
+}
+
+function destructivelyAddElementToEndOfArray() {
+destructivelyAddElementToEndOfArray.push();
+}
+
+function destructivelyRemoveElementFromBeginningOfArray() {
+
+}
+
+function removeElementFromBeginningOfArray() {
+
+}
+
+function destructivelyRemoveElementFromEndOfArray() {
+
+}
+
+function removeElementFromEndOfArray() {
+
+}
