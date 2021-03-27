3162195ce4b43426639d75d5683f8f0bf94d73d8
Shahzab Zaman <o.aszaman00@gmail.com>
2021-03-27 23:13:26 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(array, element) {
-addElementToBeginningOfArray.unshift();
-return addElementToBeginningOfArray();
+console.log(addElementToBeginningOfArray.unshift());
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
